ea8439a4896b02443e61fee96876230def87511a
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-07-05 20:04:37 -0700

    Update LocatorsSpanish.java

--- LocatorsSpanish.java
+++ LocatorsSpanish.java
@@ -1,1 +1,10 @@
+package com.CPP.bdd.pages;
+import com.qmetry.qaf.automation.vi.annotations.FindBy;
+import com.qmetry.qaf.automation.vi.webdriver.QAFExtendedWebDriver;
+import com.qmetry.qaf.automation.vi.webdriver.QAFWebELement;
+import com.CPP.core.webelements.CPPWebElement;
 
+
+
+@FindBy (Locator = "xpath=//form[@id='make-transfer-confirm-id']//div[@class='mwf-auth-text']//strong")
+  pubLic WFWebELement welcomeText;
