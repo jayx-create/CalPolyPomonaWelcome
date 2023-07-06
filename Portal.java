d7e1b61af036c39dbfea3c24bc6132fd5e080334
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2023-07-05 19:44:24 -0700

    Update Portal.java

--- Portal.java
+++ Portal.java
@@ -6,8 +6,9 @@ import io.cucumer.java.en.When;
 public class FirstSteps {
   
   
-@When("User is home page of Cal Poly Pomona portal")
-  public void userIsHomePageOfCalPolyPomonaPortal(){
+@When("User clicks on new student drop down page of Cal Poly Pomona portal")
+  public void UserClicksOnNewStudentDropDownPageOfCalPolyPomonaPortal(){
+    switch(deviceMode){
     case GlobalConstant.DESKTOP:
       switch(language){
       case GlobalConstant.ENGLISH:
@@ -17,16 +18,29 @@ public class FirstSteps {
 if(!secureSessionHeader_CPP.navigationLinks.calPolyPomonaLink.isPresent()){
   throw new CPPException.NO_ELIGIBLE_ACCOUNTS("Cal Poly Pomona menus not displayed."
       + " Please check with IT Department of problem continues."
-                                              }                                        
+                                              }   
+       secureSessionHeader_CPP.navigationLinks.calPolyPomonaLink.click();
+        navigationLinks.waitForPageToLoad();
+        navigationLinks.headerCalPolyPomona.verifyText("Cal Poly New Student")
+          }
+  }
+  }
 
     
  @Then("User can see "Welcome" text on top of portal")
-     public void userIsRegisteredWithLanguage(String language) throws Exception{
-  if(userLanguage. equalsIgnoreCase (GlobalConstant.ENGLISH)) {
-baseutil.deviceProp.setPreferredLanguage(GlobalConstant.ENGLISH);
-if(!dataType.toUpperCase().contains (GlobalConstant.DATA_TYPE_VIRTUAL)) {
-changeUserPreferredLanguage(GlobalConstant.ENGLISH_PREFFERED_LANG_CODE)
-  }
-  }
-     }
+     public void userCanSeeWelcomeTextOnTopOfPortal() throws Exception{
+   switch(deviceMode){
+   case GlobalConstant.DESKTOP:
+      switch(language){
+      case GlobalConstant.ENGLISH:
+       newStudentPage.welcomeText.isDisplayed();
+       newStudentPage.welcomeText.verifyText("Welcome");
+       break;
+     default:
+       throw new CPPException.GENERAL_SCRIPT(String.format("Invalid device type. Please Validate, deviceMode));
+   }
+ }
 }
+}
+       
+  
